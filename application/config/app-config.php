c379e03094b22e2e8910531dbda5284aef5926f8
thanhblm <thanh@beachlifemarketing.com>
2021-05-21 21:39:54 +0700

    Config staging

--- application/config/app-config.php
+++ application/config/app-config.php
@@ -50,7 +50,7 @@ define('APP_DB_PASSWORD', 'Vbrand@t2d');
 /**
  * The name of the database you want to connect to
  */
-define('APP_DB_NAME', 'crmmodbe_master');
+define('APP_DB_NAME', 'crmmodbe_staging');
 
 /**
  * @since  2.3.0
