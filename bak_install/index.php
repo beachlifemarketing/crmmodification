628ca4fb05b0c577e5fd368755ae943587cbdfe6
ETOVIET-PC <tkvclub01@hotmail.com>
2021-06-08 13:43:08 +0700

    rebuild

--- bak_install/index.php
+++ bak_install/index.php
@@ -1,5 +1,5 @@
 <?php
-
+$cleandUrl = str_replace('.', '-', $_SERVER['HTTP_HOST']);
 function xcopy($source, $dest, $permissions = 0755){
 	// Check for symlinks
 	if (is_link($source)) {
@@ -34,83 +34,82 @@ function xcopy($source, $dest, $permissions = 0755){
 }
 
 
-$file_config = APPPATH . 'config/app-config-' . str_replace('.', '-', $_SERVER['HTTP_HOST']) . '.php';
+$file_config = APPPATH . 'config/app-config-' . $cleandUrl . '.php';
 if (file_exists($file_config)) {
 	$install_url = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on' ? 'https' : 'http';
 	$install_url .= '://' . $_SERVER['HTTP_HOST'];
-	$install_url .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 	header('Location: ' . $install_url);
 	exit;
 } else {
-	if (!is_dir('../install-' . str_replace('.', '-', $_SERVER['HTTP_HOST']))) {
-		$resultCopy = xcopy('../bak_install', '../install-' . str_replace('.', '-', $_SERVER['HTTP_HOST']));
-		if ($resultCopy) {
-			$install_url = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on' ? 'https' : 'http';
-			$install_url .= '://' . $_SERVER['HTTP_HOST'];
-			$install_url .= '/install-' . str_replace('.', '-', $_SERVER['HTTP_HOST']);
-			header('Location: ' . $install_url);
-		}
-	} else {
-		$servername = "localhost";
-		$username = "servicec_thanh";
-		$password = "Vbrand@t2d";
-		$dbname = "servicec_crm";
-		try {
-			$conn = new mysqli($servername, $username, $password, $dbname);
-			if ($conn->connect_error) {
-				die("Connection failed Please contact Administrator");
-			}
-		} catch (Exception $ex) {
+	if (!is_dir('../install-' . $cleandUrl)) {
+		$resultCopy = xcopy('../bak_install', '../install-' . $cleandUrl);
+	}
+	$uri = str_replace('/', '', $_SERVER['REQUEST_URI']);
+	if (strpos($cleandUrl, $uri) == false) {
+		$install_url = isset($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) == 'on' ? 'https' : 'http';
+		$install_url .= '://' . $_SERVER['HTTP_HOST'];
+		$install_url .= '/install-' . $cleandUrl;
+		header('Location: ' . $install_url);
+	}
+	$servername = "localhost";
+	$username = "servicec_thanh";
+	$password = "Vbrand@t2d";
+	$dbname = "servicec_crm";
+	try {
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		if ($conn->connect_error) {
 			die("Connection failed Please contact Administrator");
 		}
-
-		$url = $_SERVER['HTTP_HOST'];
-		$domain_map = array();
-
-		$sql = "SELECT * FROM service";
-		$result = $conn->query($sql);
-		$nameDbApi = '';
-		$userDb = '';
-		$key = '';
-		$userPass = '';
-		$email = '';
-		if ($result->num_rows > 0) {
-			// output data of each row
-			while ($row = $result->fetch_assoc()) {
-				$nameApi = '';
-				$name_compare = '';
-				if ($row['domain'] != '') {
-					$nameApi = str_replace('.', '-', $row['domain']);
-					$name_compare = $row['domain'];
-				} else {
-					$nameApi = "crm-" . $row['id'];
-					$name_compare = $nameApi;
-				}
-
-				if (strpos($url, $name_compare) !== false) {
-					$nameDbApi = $nameApi;
-					$userDb = $row['database_name'];
-					$userPass = $row['database_password'];
-					$key = $row['key'];
-					$email = $row['service_email'];
-					break;
-				}
-			}
-		} else {
-			print_r("Please registry Service before using");
-			exit;
-		}
-
-		$conn->close();
-
-		if ($nameDbApi == '') {
-			print_r("Please registry Service before using");
-			exit;
-		}
-
-		require_once('install.class.php');
-		$install = new Install();
-		$install->go();
+	} catch (Exception $ex) {
+		die("Connection failed Please contact Administrator");
 	}
+
+	$url = $_SERVER['HTTP_HOST'];
+	$domain_map = array();
+
+	$sql = "SELECT * FROM service";
+	$result = $conn->query($sql);
+	$nameDbApi = '';
+	$userDb = '';
+	$key = '';
+	$userPass = '';
+	$email = '';
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while ($row = $result->fetch_assoc()) {
+			$nameApi = '';
+			$name_compare = '';
+			if ($row['domain'] != '') {
+				$nameApi = str_replace('.', '-', $row['domain']);
+				$name_compare = $row['domain'];
+			} else {
+				$nameApi = "crm-" . $row['id'];
+				$name_compare = $nameApi;
+			}
+
+			if (strpos($url, $name_compare) !== false) {
+				$nameDbApi = $nameApi;
+				$userDb = $row['database_name'];
+				$userPass = $row['database_password'];
+				$key = $row['key'];
+				$email = $row['service_email'];
+				break;
+			}
+		}
+	} else {
+		print_r("Please registry Service before using");
+		exit;
+	}
+
+	$conn->close();
+
+	if ($nameDbApi == '') {
+		print_r("Please registry Service before using");
+		exit;
+	}
+
+	require_once('install.class.php');
+	$install = new Install();
+	$install->go();
 	exit;
 }
